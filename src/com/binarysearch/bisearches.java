007982dbc4a346d00104dd1dc7774ec8f7357c31
Josh Cooper <cooper.s.josh@outlook.com>
2018-10-22 17:01:32 -0700

    Adds java binary search files

---
+++ src/com/binarysearch/bisearches.java
@@ -0,0 +1,19 @@
+package com.binarysearch;
+
+public class bisearches {
+    public int valuePos(int x){
+        return -1;
+    }
+
+    public int insertPos(int x){
+        return -1;
+    }
+
+    public int nextLowestPos(int x){
+        return -1;
+    }
+
+    public int nextHighestPos(int x){
+        return -1;
+    }
+}
---
+++ src/com/binarysearch/bisearchesTest.java
@@ -0,0 +1,26 @@
+package com.binarysearch;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class bisearchesTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void valuePos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insertPos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nextLowestPos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void nextHighestPos() {
+    }
+}
