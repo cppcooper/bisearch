4bcc1244bdea5006d0810b94304710e2f6c4f81a
Josh Cooper <cooper.s.josh@outlook.com>
2018-11-02 12:04:12 -0700

    Updates test suite

--- src/com/testing/bisearchTestSuite.java
+++ src/com/testing/bisearchTestSuite.java
@@ -3,6 +3,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({bisearchATest.class, bisearchBTest.class})
+@Suite.SuiteClasses({bisearchATest.class, bisearchBTest.class, bisearchCTest.class})
 public class bisearchTestSuite {
 }
